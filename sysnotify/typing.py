3989595046861da0cc810061cb33f6596520f37b
Richard Neumann <r.neumann@homeinfo.de>
2022-03-24 14:41:25 +0100

    Add SystemConnection

--- sysnotify/typing.py
+++ sysnotify/typing.py
@@ -2,11 +2,21 @@
 
 from datetime import datetime
 from ipaddress import IPv4Address, IPv6Address
-from typing import Union
+from typing import NamedTuple, Union
+
+from hwdb import System
 
 
-__all__ = ['Connections', 'IPAddress']
+__all__ = ['Connections', 'IPAddress', 'SystemConnection']
 
 
 IPAddress = Union[IPv4Address, IPv6Address]
 Connections = dict[str, dict[datetime, IPAddress]]
+
+
+class SystemConnection(NamedTuple):
+    """A system connection."""
+
+    system: System
+    timestamp: datetime
+    ip_address: IPAddress
